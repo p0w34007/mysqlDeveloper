413e88de8cc59e4aefb92c0e7e60547c96ec62ad
marcosptf <marcosptf@yahoo.com.br>
2013-11-13 17:05:05 -0200

    Update querysMSSqlServer.sql

--- querysMSSqlServer.sql
+++ querysMSSqlServer.sql
@@ -1,3 +1,34 @@
+SELECT SQL_CALC_FOUND_ROWS o.*,
+            pu as partnumber_unico,
+            puc as partnumber_unico_compacto,
+            m.empresa as marca,
+            disponibilidade_id as situacao_id,
+            oferta as status_id,
+            qtd as oferta_id,
+            partnumber as referencia
+FROM fornecedor_estoque o
+LEFT JOIN fornecedor_marca m ON o.marca_id = m.marca_id
+WHERE o.marca_id!=1
+and (
+                (pu LIKE '6200')
+            or
+                (puc LIKE '6200')
+            )
+GROUP BY o.marca_id DESC, pu
+ORDER BY o.marca_id DESC, pu,preco DESC;
+
+
+
+
+SELECT SQL_CALC_FOUND_ROWS o.*, m.empresa as marca
+FROM fornecedor_estoque o
+LEFT JOIN fornecedor_marca m ON o.marca_id = m.marca_id
+WHERE o.marca_id= and (
+(pu LIKE '6200%') or (puc LIKE '6200%')
+)
+GROUP BY o.marca_id DESC, pu
+ORDER BY o.marca_id DESC, pu,preco DESC ;
+
 
 SELECT SQL_CALC_FOUND_ROWS o.*,m.nome as marca FROM oferta o LEFT JOIN marca m ON o.marca_id = m.marca_id
 WHERE (
@@ -15,3 +46,5 @@ or
 (partnumber_unico_compacto LIKE '62022-z%')
 )
 GROUP BY status_id DESC, situacao_id,partnumber_unico,o.marca_id ORDER BY status_id DESC, situacao_id,preco DESC,partnumber_unico,o.marca_id LIMIT 90
+
+
