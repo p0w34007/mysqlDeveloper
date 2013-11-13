2e03436d7418f148517336583a1083d96d9f83f5
marcosptf <marcosptf@yahoo.com.br>
2013-11-13 17:01:36 -0200

    Delete cotfAlteracoes.sql

--- QuerysMySql/cotfAlteracoes.sql
+++
@@ -1,9 +0,0 @@
-/**********************************************************
-* @autor:Marcos Paulo
-* @since:14/06/2012
-* @desc :novas colunas para atualizar itens do CotF
-***********************************************************/
-
-ALTER TABLE fornecedor_estoque_historico ADD frete VARCHAR( 3 ) COLLATE utf8_general_ci NULL AFTER custo;
-ALTER TABLE fornecedor_estoque_historico ADD frete_valor DECIMAL( 10,2 ) COLLATE utf8_general_ci NULL AFTER frete;
-ALTER TABLE fornecedor_estoque_historico ADD frete_tempo INT( 3 ) COLLATE utf8_general_ci NULL AFTER frete_valor;
