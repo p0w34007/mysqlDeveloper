04795d7789f2982acd3888e17cbaf26bad8c4891
marcosptf <marcosptf@yahoo.com.br>
2014-10-31 16:29:27 -0200

    Update scriptMigracaoD-SaaS.sql

--- MSSqlServer/scriptMigracaoD-SaaS.sql
+++ MSSqlServer/scriptMigracaoD-SaaS.sql
@@ -33,3 +33,34 @@ select count(*) from mobile_com.dbo.SMSCustomersBK;
 select count(*) from mobile_com.dbo.SMSCustomers;
 
 /* ducplicando a tabela e inserindo o tipo identity  */
+/* ducplicando a tabela e inserindo o tipo identity  */
+sp_rename WebLog, WebLogBK;
+
+CREATE TABLE [dbo].[WebLog](
+	[LOGID] [decimal](18, 0) IDENTITY(1,1) NOT NULL,
+	[DataPost] [datetime] NULL,
+	[ConfirmationNumber] [nvarchar](255) NULL,
+	[CustomerID] [nvarchar](50) NULL,
+	[OrderID] [nvarchar](50) NULL,
+	[Email] [nvarchar](150) NULL,
+	[Request] [nvarchar](50) NULL,
+	[Status] [nvarchar](50) NULL,
+	[Mensagem] [varchar](1000) NULL,
+	[McREF] [nvarchar](100) NULL,
+	[XMLIn] [varchar](1000) NULL,
+	[XMLSend] [varchar](1000) NULL,
+	[XMLRec] [varchar](2000) NULL,
+	[XMLOut] [varchar](1000) NULL
+);
+
+truncate table mobile_com.dbo.WebLog;
+insert into mobile_com.dbo.WebLog
+(DataPost,ConfirmationNumber,CustomerID,OrderID,Email,Request,Status,Mensagem,McREF,XMLIn,XMLSend,XMLRec,XMLOut)
+select 
+DataPost,ConfirmationNumber,CustomerID,OrderID,Email,Request,Status,Mensagem,McREF,XMLIn,XMLSend,XMLRec,XMLOut
+from mobile_com.dbo.WebLogBK;
+
+select count(*) from mobile_com.dbo.WebLogBK;
+select count(*) from mobile_com.dbo.WebLog;
+
+/* ducplicando a tabela e inserindo o tipo identity  */
