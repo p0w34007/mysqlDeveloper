d09dd672c2157b97e0b1bf5cec95f3df07e167ee
marcosptf <marcosptf@yahoo.com.br>
2014-11-14 12:51:52 -0200

    Update THDBL03-parametrizacao_com.sql

--- MSSqlServer/THDBL03-parametrizacao_com.sql
+++ MSSqlServer/THDBL03-parametrizacao_com.sql
@@ -1,1 +1,27 @@
 /* parametrizacao_com  */
+sp_rename Ativacoes, AtivacoesBK;
+
+GO
+
+CREATE TABLE [dbo].[Ativacoes](
+	[SID] [numeric](18, 0) IDENTITY(1,1) NOT NULL,
+	[DataReport] [datetime] NULL,
+	[Partner] [nvarchar](50) NULL,
+	[PartnerName] [nvarchar](100) NULL,
+	[Negocio] [nvarchar](50) NULL,
+	[BaseAtivos] [nvarchar](50) NULL,
+	[Ativos] [nvarchar](50) NULL,
+	[Cancelados] [nvarchar](50) NULL
+) ON [PRIMARY]
+
+GO
+
+insert into Ativacoes
+(DataReport,Partner,PartnerName,Negocio,BaseAtivos,Ativos,Cancelados)
+select 
+DataReport,Partner,PartnerName,Negocio,BaseAtivos,Ativos,Cancelados
+from 
+AtivacoesBK;
+
+select count(*) from Ativacoes/
+select count(*) from AtivacoesBK;
