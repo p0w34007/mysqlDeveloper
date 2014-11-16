ea50d2637de77534441f1848b7fb56887555c1d5
marcosptf <marcosptf@yahoo.com.br>
2014-11-16 13:05:47 -0500

    Update THDBL03-speedy_nany_production.sql

--- MSSqlServer/THDBL03-speedy_nany_production.sql
+++ MSSqlServer/THDBL03-speedy_nany_production.sql
@@ -9,14 +9,11 @@ CREATE NONCLUSTERED INDEX [email] ON [dbo].[ISP686]
 GO
 
 
-USE [Speedy_Nany_Production]
-GO
-
 /****** Object:  Index [email]    Script Date: 14/11/2014 13:12:52 ******/
 CREATE NONCLUSTERED INDEX [email] ON [dbo].[SpeedyCustomers]
 (
 	[emailfake] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
+
 
 
