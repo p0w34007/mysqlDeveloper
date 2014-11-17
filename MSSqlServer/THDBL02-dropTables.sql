1c6e770047e5371817fa4bfe153e717806e48301
marcosptf <marcosptf@yahoo.com.br>
2014-11-17 17:10:35 -0200

    Create THDBL02-dropTables.sql

---
+++ MSSqlServer/THDBL02-dropTables.sql
@@ -0,0 +1,31 @@
+USE [Speedy_Nany_Production];
+GO
+drop table [dbo].[PIUsuario];
+GO
+drop table [dbo].[SpeedyAFO];
+GO
+drop table [dbo].[SpeedyAFOExportado];
+GO
+drop table CREATE TABLE [dbo].[SpeedyCritica];
+GO
+drop table [dbo].[SpeedyCustomers];
+GO
+drop table [dbo].[SpeedyFiles];
+GO
+drop table [dbo].[SpeedyFiltro];
+GO
+drop table [dbo].[SpeedyJob];
+GO
+drop table [dbo].[SpeedyLog];
+GO
+drop table [dbo].[SpeedyLogEnvio];
+GO
+drop table [dbo].[SpeedyOrders];
+GO
+drop table [dbo].[SpeedySER];
+GO
+drop table [dbo].[SpeedySVA];
+GO
+drop table [dbo].[SpeedyTrial];
+GO
+drop table [dbo].[SpeedyVencimentos];
