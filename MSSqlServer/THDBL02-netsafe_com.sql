65c79dfa6d3883be9e0e7bd937aa7539789b4a9d
marcosptf <marcosptf@yahoo.com.br>
2014-11-21 17:20:26 -0200

    Update THDBL02-netsafe_com.sql

--- MSSqlServer/THDBL02-netsafe_com.sql
+++ MSSqlServer/THDBL02-netsafe_com.sql
@@ -2,6 +2,172 @@
 USE [netsafe_com]
 GO
 
+/****** Object:  View [dbo].[Geral_Novo_BR]    Script Date: 21/11/2014 17:19:59 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[Geral_Novo_BR]
+AS
+SELECT     dbo.Orders_BR.SubPartnerID, dbo.Orders_BR.McafeeOrderID, dbo.Customers.Email, dbo.Orders_BR.CustomerID, dbo.Orders_BR.PartnerOrderID, 
+                      dbo.Orders_BR.RequestType, dbo.Orders_BR.ProductName, dbo.Orders_BR.OrderDate, dbo.Orders_BR.OrderEnd, dbo.Orders_BR.ProductID, 
+                      dbo.Orders_BR.StatusMcafee, dbo.Orders_BR.Status, dbo.Orders_BR.Offer
+FROM         dbo.Orders_BR LEFT OUTER JOIN
+                      dbo.Customers ON dbo.Orders_BR.CustomerID = dbo.Customers.CustomerID AND dbo.Orders_BR.SubPartnerID = dbo.Customers.SubPartnerID
+
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[41] 4[20] 2[13] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Orders_BR"
+            Begin Extent = 
+               Top = 11
+               Left = 30
+               Bottom = 299
+               Right = 202
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Customers"
+            Begin Extent = 
+               Top = 14
+               Left = 407
+               Bottom = 297
+               Right = 608
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 16
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 4530
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 2310
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Geral_Novo_BR'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Geral_Novo_BR'
+GO
+
+
+
+
+
+
+USE [netsafe_com]
+GO
+
 /****** Object:  View [dbo].[Geral_Grupo_iG_Oi]    Script Date: 21/11/2014 17:18:50 ******/
 SET ANSI_NULLS ON
 GO
