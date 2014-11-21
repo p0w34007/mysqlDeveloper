4bc039c3fcada110ef72526c09b7e462a7f87b66
marcosptf <marcosptf@yahoo.com.br>
2014-11-21 16:23:04 -0200

    Update THDBL02-netsafe_com.sql

--- MSSqlServer/THDBL02-netsafe_com.sql
+++ MSSqlServer/THDBL02-netsafe_com.sql
@@ -1,3 +1,17 @@
+
+USE [netsafe_com]
+GO
+
+/****** Object:  Index [datapost]    Script Date: 21/11/2014 16:22:44 ******/
+CREATE NONCLUSTERED INDEX [datapost] ON [dbo].[weblog]
+(
+	[DataPost] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+
+
+
 USE [netsafe_com]
 GO
 
