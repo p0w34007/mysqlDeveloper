f80bf47144926f13b563646f665750970aef2bc5
marcosptf <marcosptf@yahoo.com.br>
2014-11-21 17:21:08 -0200

    Update THDBL02-netsafe_com.sql

--- MSSqlServer/THDBL02-netsafe_com.sql
+++ MSSqlServer/THDBL02-netsafe_com.sql
@@ -1,3 +1,204 @@
+USE [netsafe_com]
+GO
+
+/****** Object:  View [dbo].[Geral-BR]    Script Date: 21/11/2014 17:20:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[Geral-BR]
+AS
+SELECT     TOP 100 PERCENT dbo.Customers.SubPartnerID, dbo.Orders.McafeeOrderID, dbo.Customers.Email, dbo.Customers.CustomerID, dbo.Orders.PartnerOrderID, 
+                      dbo.Orders.RequestType, dbo.Products.ProductName, dbo.Orders.OrderDate, dbo.Orders.OrderEnd, dbo.OrderItem.ProductID, dbo.Products.Offer, dbo.Orders.Status, 
+                      dbo.Orders.NetSafeOrderID, dbo.Orders.StatusMcafee, dbo.Customers.FirstName, dbo.Customers.LastName
+FROM         dbo.Customers INNER JOIN
+                      dbo.Orders INNER JOIN
+                      dbo.OrderItem ON dbo.Orders.NetSafeOrderID = dbo.OrderItem.OrderID AND dbo.Orders.SubPartnerID = dbo.OrderItem.SubPartnerID INNER JOIN
+                      dbo.Products ON dbo.OrderItem.ProductID = dbo.Products.SKU ON dbo.Customers.SubPartnerID = dbo.Orders.SubPartnerID AND 
+                      dbo.Customers.CustomerID = dbo.Orders.CustomerID
+GROUP BY dbo.Customers.SubPartnerID, dbo.Customers.Email, dbo.Customers.CustomerID, dbo.Orders.RequestType, dbo.Products.ProductName, dbo.Orders.OrderDate, 
+                      dbo.Orders.OrderEnd, dbo.OrderItem.ProductID, dbo.Products.Offer, dbo.Orders.Status, dbo.Orders.NetSafeOrderID, dbo.Orders.StatusMcafee, 
+                      dbo.Orders.McafeeOrderID, dbo.Orders.PartnerOrderID, dbo.Customers.FirstName, dbo.Customers.LastName
+HAVING      (dbo.Orders.Status = N'released' OR
+                      dbo.Orders.Status = N'warning') AND (dbo.Orders.StatusMcafee LIKE N'%0000%' OR
+                      dbo.Orders.StatusMcafee LIKE N'%1000%' OR
+                      dbo.Orders.StatusMcafee LIKE N'%500%' AND NOT (dbo.Orders.StatusMcafee LIKE N'%malformed%')) AND (dbo.Orders.RequestType = N'new') AND 
+                      (dbo.Orders.OrderDate < CONVERT(DATETIME, '2010-08-01 00:00:00', 102)) AND (dbo.Customers.SubPartnerID = N'netbr99' OR
+                      dbo.Customers.SubPartnerID = N'psbr99') AND (dbo.Orders.OrderEnd = dbo.Orders.OrderDate)
+ORDER BY dbo.Orders.McafeeOrderID, dbo.Orders.RequestType DESC, dbo.Customers.CustomerID, dbo.Orders.PartnerOrderID, dbo.OrderItem.ProductID, dbo.Orders.OrderDate
+
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[35] 4[24] 2[17] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1[32] 4[23] 3) )"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 1
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Customers"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 123
+               Right = 239
+            End
+            DisplayFlags = 280
+            TopColumn = 9
+         End
+         Begin Table = "Orders"
+            Begin Extent = 
+               Top = 6
+               Left = 277
+               Bottom = 241
+               Right = 464
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "OrderItem"
+            Begin Extent = 
+               Top = 6
+               Left = 502
+               Bottom = 243
+               Right = 682
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Products"
+            Begin Extent = 
+               Top = 126
+               Left = 38
+               Bottom = 243
+               Right = 212
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+      PaneHidden = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 17
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 2430
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1665
+         Width = 2430
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 2760
+         Width = 1500
+         Width = 2985
+         Width = 2985
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 12
+         Column = 1440
+         Alias = 1455
+         Table = 1170
+         Output = 720
+         Append =' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Geral-BR'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N' 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 6780
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Geral-BR'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=2 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Geral-BR'
+GO
+
+
+
 
 USE [netsafe_com]
 GO
