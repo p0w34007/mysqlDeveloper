08b4fd7b66d40fe971a2b9f8d77fc4f668d1f25d
marcosptf <marcosptf@yahoo.com.br>
2014-11-21 17:41:16 -0200

    Update THDBL02-netsafe_com.sql

--- MSSqlServer/THDBL02-netsafe_com.sql
+++ MSSqlServer/THDBL02-netsafe_com.sql
@@ -1,6 +1,202 @@
 USE [netsafe_com]
 GO
 
+/****** Object:  View [dbo].[Orders_BR]    Script Date: 21/11/2014 17:40:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[Orders_BR]
+AS
+SELECT     TOP 100 PERCENT dbo.Orders.SubPartnerID, dbo.Orders.McafeeOrderID, dbo.Orders.CustomerID, dbo.Orders.PartnerOrderID, dbo.Orders.RequestType, 
+                      dbo.Orders.OrderDate, dbo.Orders.OrderEnd, dbo.Orders.Status, dbo.Orders.NetSafeOrderID, dbo.Orders.StatusMcafee, dbo.OrderItem.OrderID, 
+                      dbo.OrderItem.ProductID, dbo.OrderItem.ProductName, dbo.Products.Offer
+FROM         dbo.Products RIGHT OUTER JOIN
+                      dbo.OrderItem ON dbo.Products.SKU = dbo.OrderItem.ProductID RIGHT OUTER JOIN
+                      dbo.Orders INNER JOIN
+                      dbo.Filtro_BR_Mauricio ON dbo.Orders.SubPartnerID = dbo.Filtro_BR_Mauricio.Fil_Parceiro AND dbo.Orders.OrderDate < dbo.Filtro_BR_Mauricio.Fil_Data_Corte ON 
+                      dbo.OrderItem.OrderID = dbo.Orders.NetSafeOrderID
+WHERE     (dbo.Orders.RequestType = N'new') AND (dbo.Orders.Status = N'released' OR
+                      dbo.Orders.Status = N'warning') AND (dbo.Orders.OrderEnd = dbo.Orders.OrderDate OR
+                      dbo.Orders.OrderEnd > dbo.Filtro_BR_Mauricio.Fil_Data_Cancel) AND (dbo.Orders.StatusMcafee LIKE N'%1000%' OR
+                      dbo.Orders.StatusMcafee LIKE N'%5001%') AND (dbo.Products.Offer <> N'30 Day Free Trial')
+ORDER BY dbo.Orders.McafeeOrderID, dbo.Orders.RequestType DESC, dbo.Orders.PartnerOrderID, dbo.Orders.OrderDate
+
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[41] 4[25] 2[15] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Orders"
+            Begin Extent = 
+               Top = 5
+               Left = 214
+               Bottom = 318
+               Right = 385
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Filtro_BR_Mauricio"
+            Begin Extent = 
+               Top = 10
+               Left = 6
+               Bottom = 118
+               Right = 159
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "OrderItem"
+            Begin Extent = 
+               Top = 7
+               Left = 423
+               Bottom = 318
+               Right = 603
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Products"
+            Begin Extent = 
+               Top = 6
+               Left = 641
+               Bottom = 316
+               Right = 815
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 18
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 2730
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1485
+         Width = 2580
+         Width = 3600
+         Width = 2310
+         Width = 1500
+         Width = 2805
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+       ' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Orders_BR'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N'  Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 6690
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Orders_BR'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=2 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Orders_BR'
+GO
+
+
+
+USE [netsafe_com]
+GO
+
 /****** Object:  View [dbo].[NETCP99-xpress]    Script Date: 21/11/2014 17:39:59 ******/
 SET ANSI_NULLS ON
 GO
