8a10a2fe26c5b5d7f66d54d5a9c365fd23b5e7ee
marcosptf <marcosptf@yahoo.com.br>
2014-11-21 16:56:28 -0200

    Update THDBL02-netsafe_com.sql

--- MSSqlServer/THDBL02-netsafe_com.sql
+++ MSSqlServer/THDBL02-netsafe_com.sql
@@ -1,4 +1,196 @@
 
+USE [netsafe_com]
+GO
+
+/****** Object:  View [dbo].[_Total_Cancel]    Script Date: 21/11/2014 16:56:04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[_Total_Cancel]
+AS
+SELECT     TOP 100 PERCENT dbo.Customers.SubPartnerID, dbo.Orders.RequestType, dbo.Products.ProductName, COUNT(DISTINCT dbo.Orders.PartnerOrderID) AS Total
+FROM         dbo.Customers INNER JOIN
+                      dbo.Orders INNER JOIN
+                      dbo.OrderItem ON dbo.Orders.NetSafeOrderID = dbo.OrderItem.OrderID AND dbo.Orders.SubPartnerID = dbo.OrderItem.SubPartnerID INNER JOIN
+                      dbo.Products ON dbo.OrderItem.ProductID = dbo.Products.SKU ON dbo.Customers.SubPartnerID = dbo.Orders.SubPartnerID AND 
+                      dbo.Customers.CustomerID = dbo.Orders.CustomerID
+WHERE     (dbo.Orders.Status = N'released' OR
+                      dbo.Orders.Status = N'warning') AND (dbo.Orders.StatusMcafee LIKE N'%1000%' OR
+                      dbo.Orders.StatusMcafee LIKE N'%500%') AND (dbo.Orders.OrderEnd BETWEEN CONVERT(DATETIME, '2010-03-01 00:00:00', 102) AND CONVERT(DATETIME, 
+                      '2010-04-01 00:00:00', 102))
+GROUP BY dbo.Customers.SubPartnerID, dbo.Orders.RequestType, dbo.Products.ProductName
+HAVING      (dbo.Orders.RequestType = N'cancel') AND (dbo.Customers.SubPartnerID = N'netbr17' OR
+                      dbo.Customers.SubPartnerID = N'netbr22' OR
+                      dbo.Customers.SubPartnerID = N'netbr23')
+ORDER BY dbo.Customers.SubPartnerID, dbo.Orders.RequestType
+
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[21] 4[40] 2[6] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1[26] 4[40] 3) )"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4[30] 2[40] 3) )"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4[50] 3) )"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 3
+   End
+   Begin DiagramPane = 
+      PaneHidden = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Customers"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 125
+               Right = 255
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Orders"
+            Begin Extent = 
+               Top = 6
+               Left = 293
+               Bottom = 125
+               Right = 496
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "OrderItem"
+            Begin Extent = 
+               Top = 6
+               Left = 534
+               Bottom = 125
+               Right = 730
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Products"
+            Begin Extent = 
+               Top = 126
+               Left = 38
+               Bottom = 245
+               Right = 228
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 9
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 3210
+         Width = 1500
+         Width = 1665
+         Width = 1665
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 12
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 930
+         GroupBy = 1350
+         Filter = 3555
+         Or = 1350
+         Or = 1350
+         Or = ' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'_Total_Cancel'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N'1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'_Total_Cancel'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=2 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'_Total_Cancel'
+GO
+
+
+
+
+
 sp_rename weblog, weblogBK;
 
 CREATE TABLE [dbo].[weblog](
