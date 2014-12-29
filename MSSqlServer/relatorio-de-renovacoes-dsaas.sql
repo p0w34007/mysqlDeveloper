18641c08ae1abea6a607c06eb3c858ffef6c0ebe
marcosptf <marcosptf@yahoo.com.br>
2014-12-29 12:21:51 -0200

    Update relatorio-de-renovacoes-dsaas.sql

--- MSSqlServer/relatorio-de-renovacoes-dsaas.sql
+++ MSSqlServer/relatorio-de-renovacoes-dsaas.sql
@@ -1,6 +1,7 @@
 /****** Script do comando SelectTopNRows de SSMS  ******/
 --cabecalho do csv exportado
-PSID OrderID CustomerID NomeProduto Periodo Maquinas DataOrdem TipoOrdem StatusOrdem EmailAddress_adm DDD Phone FirstName LastName CompanyName AddressLine1 AddressLine2 Suite CityName StateProvinceCD PostalCD
+PSID	OrderID	CustomerID	NomeProduto	Periodo	Maquinas	DataOrdem	TipoOrdem	StatusOrdem	EmailAddress_adm	DDD	Phone	FirstName	LastName	CompanyName	AddressLine1	AddressLine2	Suite	CityName	StateProvinceCD	PostalCD
+
 
 --query para gerar relatorio
 SELECT		[PSID]
