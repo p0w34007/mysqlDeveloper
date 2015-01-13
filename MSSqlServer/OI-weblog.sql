07f501b1a185fa77013d14f79ad617a680e9e0b3
marcosptf <marcosptf@yahoo.com.br>
2015-01-13 12:25:20 -0200

    Update OI-weblog.sql

--- MSSqlServer/OI-weblog.sql
+++ MSSqlServer/OI-weblog.sql
@@ -1,3 +1,4 @@
+/*  quantidade de multidevices por periodo */
 SELECT count(*)
   FROM [mobile_com].[dbo].[WebLog]
   where datapost > '2015-01-12' and datapost < '2015-01-13'
@@ -8,3 +9,30 @@ SELECT count(*)
  xmlin like '%907-66226-srvc_1umma%' or
  xmlin like '%907-66193-3dumma%')
  ;
+
+ 
+ /* media de requisicoes por minuto */
+ select      /* top 100 */
+			count(logid) as quantidade,
+			/* cast(datapost as varchar(25)), */
+			substring(convert(varchar(19),datapost,120),1,16) as media_horario
+			--cast(datapost as varchar(19)) as formato
+from		[mobile_com].[dbo].[WebLog]
+where       datapost > '2015-01-13 00:00:00'
+and			( 
+	confirmationnumber like '%BR61%' or  
+	confirmationnumber like '%BR62%' or  
+	confirmationnumber like '%BR55%'
+	)
+ and ( 
+	xmlin like '%907-61081-1usermds%'	or
+	xmlin like '%907-60468-mds%'		or
+	xmlin like '%907-66226-srvc_1umma%' or
+	xmlin like '%907-66193-3dumma%'
+) 
+--group by substring(DataPost,12,5)
+group by substring(convert(varchar(19),datapost,120),1,16)
+order by 2 desc;
+
+
+
