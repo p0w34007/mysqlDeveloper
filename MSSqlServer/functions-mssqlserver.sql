64d53c873502859bf2ad18768e1481ed131cc2be
p0w34007 <p0w34007@yahoo.com>
2015-03-05 10:18:26 -0500

    Update functions-mssqlserver.sql

--- MSSqlServer/functions-mssqlserver.sql
+++ MSSqlServer/functions-mssqlserver.sql
@@ -25,3 +25,36 @@ and			(
 --group by substring(DataPost,12,5)
 group by substring(convert(varchar(19),datapost,120),1,16)
 order by 2 desc;
+
+
+
+/* function split to sql server 2008 */
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE FUNCTION [dbo].[Split]  
+(  
+ @RowData nvarchar(MAX),
+ @SplitOn nvarchar(5)
+)    
+RETURNS @ReturnValue TABLE   
+(Data NVARCHAR(MAX))   
+AS
+BEGIN
+ Declare @Counter int
+ Set @Counter = 1 
+ While (Charindex(@SplitOn,@RowData)>0) 
+ Begin  
+  Insert Into @ReturnValue (data)  
+  Select Data = 
+      ltrim(rtrim(Substring(@RowData,1,Charindex(@SplitOn,@RowData)-1)))
+  Set @RowData = 
+      Substring(@RowData,Charindex(@SplitOn,@RowData)+1,len(@RowData)) 
+  Set @Counter = @Counter + 1  
+ End 
+ Insert Into @ReturnValue (data)  
+ Select Data = ltrim(rtrim(@RowData))  
+ Return  
+END
+
